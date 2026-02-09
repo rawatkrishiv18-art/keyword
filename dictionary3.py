c5e314939dd639cca3ae97805b27552e13be5aa2
rawatkrishiv18-art <rawatkrishiv18@gmail.com>
2026-02-09 19:42:18 +0530

    Add files via upload

---
+++ dictionary3.py
@@ -0,0 +1,7 @@
+spcmcountry_code = {'india': '0091',  'australia': '0025', 'nepal': '00977'}
+
+print("country code of india is: " )
+print(spcmcountry_code.get('india', 'not found'))
+
+print("country code of japan is: " )
+print(spcmcountry_code.get('japan', 'not found'))
