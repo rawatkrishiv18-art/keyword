d2e00a2cf0bbd6d82f6e180260dde569ff4092a4
rawatkrishiv18-art <rawatkrishiv18@gmail.com>
2026-02-09 19:32:32 +0530

    Add files via upload

---
+++ dictionary1.py
@@ -0,0 +1,32 @@
+student_data = {'id1':
+    {'name':['sara'],
+     'class':['V'],
+     'subject_integration':['english','maths','science']
+     },
+     
+    'id2':
+    {'name':['david'],
+      'class':['V'],
+      'subject_integration':['english','maths','science']
+     },
+
+    'id3':
+    {'name':['sara'],
+      'class':['V'],
+      'subject_integration':['english','maths','science']
+     },
+
+    'id4':
+    {'name':['surya'],
+      'class':['V'],
+      'subject_integration':['english','maths','science']
+     },
+
+}
+
+result = {}
+for key, value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
+print(result)
---
+++ dictionary2.py
@@ -0,0 +1,10 @@
+test_dict = {'codingal': 2, 'is' : 2, 'best': 2 , 'for' : 2, 'coding' : 1}
+print("Original Dictionary:",str(test_dict))
+
+k=2
+res=0
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res +1
+
+print("frequency of k is:" + str(res))
